3c04323541e293e63c016831bac357c658724243
VanThen <nguyenvthang2409@gmail.com>
2025-05-10 04:13:11 +0700

    update simple_user

--- core/simple_user.go
+++ core/simple_user.go
@@ -6,19 +6,20 @@ type SimpleUser struct {
 	Email     string `json:"email" gorm:"column:email" db:"email"`
 	LastName  string `json:"last_name" gorm:"column:last_name;" db:"last_name"`
 	FirstName string `json:"first_name" gorm:"column:first_name;" db:"first_name"`
-	Avatar    *Image `json:"avatar" gorm:"column:avatar;" db:"avatar"`
+	AvatarId  int    `json:"-" gorm:"column:avatar_id" db:"avatar_id"`
+	Avatar    *Image `json:"avatar" gorm:"-" db:"-"`
 }
 
 func (SimpleUser) TableName() string {
 	return "users"
 }
 
-func NewSimpleUser(id int, email, firstName, lastName string, avatar *Image) SimpleUser {
+func NewSimpleUser(id int, email, firstName, lastName string, avatarId int) SimpleUser {
 	return SimpleUser{
 		SQLModel:  SQLModel{Id: id},
 		Email:     email,
 		LastName:  lastName,
 		FirstName: firstName,
-		Avatar:    avatar,
+		AvatarId:  avatarId,
 	}
 }
